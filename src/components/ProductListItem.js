8f4e8acee35322ca6d8419f150dd060bd85f2fa1
rakibtg <rakibtg@gmail.com>
2020-11-07 17:51:12 +0600

    Show a single product

---
+++ src/components/ProductListItem.js
@@ -0,0 +1,36 @@
+import { Pane, Text } from "evergreen-ui";
+import Link from "../components/Link";
+import Colors from "../constants/Colors";
+
+const ProductListItem = ({ product }) => {
+  const { id, name, brand, price } = product;
+
+  return (
+    <Link to={`/view/${id}`} textDecoration="none">
+      <Pane
+        clearfix
+        padding={10}
+        elevation={0}
+        marginBottom={10}
+        hoverElevation={1}
+        background="tint1"
+      >
+        <Pane>
+          <Text size={500} color={Colors.blueBase} fontWeight={500}>
+            {name}
+          </Text>
+        </Pane>
+        <Pane>
+          <Text size={400} color={Colors.redBase} fontWeight={700}>
+            ${price}
+          </Text>
+          <Text size={400} marginLeft={10}>
+            {brand}
+          </Text>
+        </Pane>
+      </Pane>
+    </Link>
+  );
+};
+
+export default ProductListItem;
