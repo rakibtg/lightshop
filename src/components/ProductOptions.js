3dd120248978f3ae919fe47e8e44a8857af5d3c7
Kazi Mehedi Hasan <rakibtg@gmail.com>
2020-11-08 00:22:08 +0600

    Simplified logic, show items with available quantity first

--- src/components/ProductOptions.js
+++ src/components/ProductOptions.js
@@ -1,72 +1,12 @@
+import { Pane, Text } from "evergreen-ui";
 import { useState, useEffect } from "react";
 import { useSelector, shallowEqual, useDispatch } from "react-redux";
-import { Select } from "evergreen-ui";
-import { setProductViewSelectedOptions } from "../store/Actions";
-import upperFirst from "../helpers/upperFirst";
-import DataCell from "../components/DataCell";
 
-const ColorSelector = ({ options, updateOptionsValue, setSelectedOption }) => {
-  const availableColors = options.map((op) => op.color);
-  const handleOptionSelection = (event) => {
-    const selectedColor = event.target.value.toLowerCase();
-    const selectedOption = options.find((op) => op.color === selectedColor);
-    updateOptionsValue({
-      ...selectedOption,
-      quantity: selectedOption.quantity > 1 ? 1 : selectedOption.quantity,
-    });
-    setSelectedOption(selectedOption);
-  };
-  return (
-    <DataCell label="Color">
-      <Select width={100} onChange={handleOptionSelection}>
-        {availableColors.map((color, index) => {
-          return <option key={index}>{upperFirst(color)}</option>;
-        })}
-      </Select>
-    </DataCell>
-  );
-};
+import { setActiveProductRequirements } from "../store/Actions";
 
-const GenericOptionSelector = ({
-  label,
-  updatable,
-  options,
-  updateOptionsValue,
-}) => {
-  const handleGenericOptionChange = (event) => {
-    updateOptionsValue({
-      [updatable]: Number(event.target.value),
-    });
-  };
-  return (
-    <DataCell label={label}>
-      <Select width={100} onChange={handleGenericOptionChange}>
-        {options.map((option, index) => {
-          return <option key={index}>{option}</option>;
-        })}
-      </Select>
-    </DataCell>
-  );
-};
-
-const QuantitySelector = ({ quantity, updateOptionsValue }) => {
-  const options = new Array(quantity).fill(0);
-  const handleQuantityChange = (event) => {
-    updateOptionsValue({
-      quantity: Number(event.target.value),
-    });
-  };
-  return (
-    <DataCell label="Quantity">
-      <Select width={100} onChange={handleQuantityChange}>
-        {options.map((option, index) => {
-          const value = index + 1;
-          return <option key={value}>{value}</option>;
-        })}
-      </Select>
-    </DataCell>
-  );
-};
+import ColorSelector from "./ColorSelector";
+import GenericOptionSelector from "./GenericOptionSelector";
+import QuantitySelector from "./QuantitySelector";
 
 const ProductOptions = ({ options }) => {
   const dispatch = useDispatch();
@@ -76,59 +16,54 @@ const ProductOptions = ({ options }) => {
     shallowEqual
   );
 
-  const updateOptionsValue = (option) => {
-    const nextOption = {};
-    Object.keys(option).forEach((property) => {
-      const value = option[property];
-      nextOption[property] =
-        typeof value === "object" && value.length ? value[0] : value;
-    });
-    dispatch(setProductViewSelectedOptions(nextOption));
-  };
-
   useEffect(() => {
     if (!selectedOption) {
-      const _option = options[0];
-      updateOptionsValue({
-        ..._option,
-        quantity: _option.quantity > 1 ? 1 : _option.quantity,
-      });
-      // dispatch(setProductViewSelectedOptions(_option));
+      const inStock = options.find((op) => op.quantity > 0);
+      const _option = inStock ? inStock : options[0];
+      dispatch(
+        setActiveProductRequirements({
+          ..._option,
+          quantity: _option.quantity > 1 ? 1 : _option.quantity,
+        })
+      );
       setSelectedOption(_option);
     }
-  }, [selectedOption, setSelectedOption, options]);
+  }, [selectedOption, setSelectedOption, options, dispatch]);
 
-  console.log(options);
   if (!selectedOption) {
-    return <div>Please wait</div>;
+    return (
+      <Pane>
+        <Text>Please wait</Text>
+      </Pane>
+    );
   }
   return (
     <div>
-      <pre>{JSON.stringify(selectedOption, null, 4)}</pre>
+      <pre>{JSON.stringify({ selectedOption, selections }, null, 4)}</pre>
       <ColorSelector
         options={options}
-        updateOptionsValue={updateOptionsValue}
         setSelectedOption={setSelectedOption}
+        value={selections.color}
       />
       {selectedOption.hasOwnProperty("power") && (
         <GenericOptionSelector
           label="Power"
           updatable="power"
           options={selectedOption.power}
-          updateOptionsValue={updateOptionsValue}
+          value={selections.power}
         />
       )}
       {selectedOption.hasOwnProperty("storage") && (
         <GenericOptionSelector
           label="Storage"
           options={selectedOption.storage}
-          updateOptionsValue={updateOptionsValue}
+          value={selections.storage}
         />
       )}
-      {selectedOption.quantity && (
+      {selectedOption.hasOwnProperty("quantity") && (
         <QuantitySelector
           quantity={selectedOption.quantity}
-          updateOptionsValue={updateOptionsValue}
+          value={selections.quantity}
         />
       )}
     </div>
