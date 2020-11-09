f433501af2a03ebfe1ada355d96aff9c16e58c6f
rakibtg <rakibtg@gmail.com>
2020-11-09 10:55:20 +0600

    Show product availability

---
+++ src/components/Availability.js
@@ -0,0 +1,22 @@
+import { Pane, Text } from "evergreen-ui";
+import DataCell from "../components/DataCell";
+import Colors from "../constants/Colors";
+
+const Availability = ({ status }) => {
+  return (
+    <DataCell label="Availability">
+      <Pane display="flex" alignItems="center">
+        <Pane
+          width={10}
+          height={10}
+          backgroundColor={status ? Colors.greenBase : Colors.redBase}
+          borderRadius={10}
+          marginRight={5}
+        ></Pane>
+        <Text>{!status && "Not "}Available</Text>
+      </Pane>
+    </DataCell>
+  );
+};
+
+export default Availability;
