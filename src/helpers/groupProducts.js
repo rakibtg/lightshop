398831f60cd176d936b0933f2bd0fc59f925cfa2
rakibtg <rakibtg@gmail.com>
2020-11-10 17:22:25 +0600

    Added maxQuantity property with each cart item

--- src/helpers/groupProducts.js
+++ src/helpers/groupProducts.js
@@ -2,6 +2,11 @@ const groupProducts = (cartItems, products) => {
   const _group = {};
   cartItems.forEach((item, index) => {
     item = { ...item, cartItemIndex: index };
+    const product = products.find((p) => p.id === item.productId);
+    const { quantity: maxQuantity } = product.options.find(
+      (op) => op.color === item.color
+    );
+    item = { ...item, maxQuantity };
     if (_group.hasOwnProperty(item.productId)) {
       _group[item.productId] = {
         ..._group[item.productId],
@@ -9,7 +14,6 @@ const groupProducts = (cartItems, products) => {
       };
     } else {
       // Add the product to the group.
-      const product = products.find((p) => p.id === item.productId);
       _group[item.productId] = {
         ...product,
         cart: [item],
